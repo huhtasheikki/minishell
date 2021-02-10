1edc3c8ab43cf47462cd0fc3c8ee8a95376b2444
hhuhtane <hhuhtane@student.hive.fi>
2021-02-10 12:03:14 +0200

    Clean signal.c

--- srcs/signal.c
+++ srcs/signal.c
@@ -6,7 +6,7 @@
 /*   By: hhuhtane <hhuhtane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 11:17:54 by hhuhtane          #+#    #+#             */
-/*   Updated: 2021/02/10 11:58:40 by hhuhtane         ###   ########.fr       */
+/*   Updated: 2021/02/10 12:02:54 by hhuhtane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 void	sig_int(int signo)
 {
-	ft_printf("\n%d\n", signo);
+	ft_printf("\n");
+	(void)signo;
 	if (g_pid != 0)
 		kill(g_pid, 0);
 	else
